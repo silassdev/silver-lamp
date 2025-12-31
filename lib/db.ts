0eb046535e3c6831aaa9a76468c381e0b1f77d74
Silas Tyokaha <3tyokaha@gmail.com>
2025-12-31 12:32:42 +0100

    Removed .local in env URI

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
-if (!MONGODB_URI) throw new Error("Define MONGODB_URI in .env.local");
+if (!MONGODB_URI) throw new Error("Define MONGODB_URI in .env");
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
